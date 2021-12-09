acfad1921218e6054e67cdfda5ec6c6067e54c77
Yauhen Davidovich <davidovich336@gmail.com>
2021-12-09 18:46:09 +0100

    feat: add getUsers api

---
+++ src/dal/usersApi.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL:
+        'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data',
+});
+
+export interface Users {
+    id:       number;
+    name:     string;
+    username: string;
+    email:    string;
+    address:  Address;
+    phone:    string;
+    website:  string;
+    company:  Company;
+}
+
+export interface Address {
+    street:  string;
+    suite:   string;
+    city:    string;
+    zipcode: string;
+    geo:     Geo;
+}
+
+export interface Geo {
+    lat: string;
+    lng: string;
+}
+
+export interface Company {
+    name:        string;
+    catchPhrase: string;
+    bs:          string;
+}
+
+
+export const usersAPI = {
+    getUsers() {
+        return instance.get<Users[]>("");
+    },
+};
+
